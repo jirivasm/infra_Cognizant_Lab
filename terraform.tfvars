f656b03b75af28d474d39f8101b83cc0d3803dfc
ravindranath barathy <ravindranath.barathy@yahoo.com>
2022-09-15 08:15:29 -0400

    Initial

---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+org_name   = "cognizant"
+team_name  = "revature"
+project_id = "2022"
+region     = ""
+env = {
+  "dev" = "dev"
+  "qa"  = "qa"
+}
+codebuild_compute_type = "BUILD_GENERAL1_MEDIUM"
+codebuild_image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+codebuild_type         = "LINUX_CONTAINER"
+codecommit_branch      = "master"
---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "infra-state"
+    key            = "infra"
+    region         = "us-east-1"
+  }
+  
+  required_version = ">= 0.15"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.1"
+      #Will allow installation of 4.15.1 and 4.15.10 but not 4.16.0
+    }
+  }
+}
